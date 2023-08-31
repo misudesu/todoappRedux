b09bcb7cfb979ca5b2beb89a0596c7c205cf8b82
misudesu <misaeldessalegn@gmail.com>
2023-08-31 14:07:08 +0300

    go live bro

--- src/Component/Todo.js
+++ src/Component/Todo.js
@@ -5,7 +5,7 @@ import { removeTodo, updateTodo } from '../Features/todo/todoSlice';
 function Todo(props) {
     const dispatch=useDispatch();
     const todos =useSelector(state=>state.todos)
-    const [text, setNewText] = useState('');
+
 
   return (
     <div>
@@ -16,9 +16,9 @@ function Todo(props) {
                 <div className='flex justify-between items-center gap-4 px-2 py-1 border-b border-cyan-100 bg-slate-500 text-white'>
 
 <p>{todo.text}</p>
-<input className='text-black' type="text" value={text} onChange={(e)=>setNewText(e.target.value)} />
+
 <button onClick={()=>dispatch(removeTodo(todo.id))} className='bg-red-800 px-2 py-1 rounded-md'>Delete</button>
-<button onClick={()=>dispatch(updateTodo( {id:todo.id,text:text}))} className='bg-red-800 px-2 py-1 rounded-md'>Update</button>
+<button onClick={()=>dispatch(updateTodo( {id:todo.id,text:props.input}))} className='bg-red-800 px-2 py-1 rounded-md'>Update</button>
                 </div>
 
             </li>
